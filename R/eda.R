39e8db700aa32a8e6c87e86cb0b615956575f504
gstephan30 <gstephan30@gmail.com>
2021-11-26 11:06:20 +0100

    added new week

--- R/eda.R
+++ R/eda.R
@@ -5,6 +5,7 @@ library(readr)
 library(dplyr)
 library(stringr)
 library(purrr)
+library(tidyr)
 
 
 # get recent files
@@ -29,3 +30,20 @@ df$weeks_countries %>%
 df$weeks_countries %>% 
   filter(weekly_rank == 1,
          show_title == "Arcane")
+
+df$weeks_global %>% 
+  filter(grepl("Squid|Arcane", show_title))
+
+read_tsv("data_raw/20211117_weeks_global.tsv") %>% 
+  filter(grepl("Squid|Arcane", show_title))
+
+# explore weekly hours viewed
+df$weeks_global %>% 
+  filter(cumulative_weeks_in_top_10 %in% c(1, 2)) %>%
+  pivot_wider(names_from = cumulative_weeks_in_top_10,
+              values_from = weekly_hours_viewed,
+              values_fill = 0) %>% 
+  janitor::clean_names() %>%
+  mutate(ind = x1>x2) %>% 
+  filter(ind == FALSE, x1 != 0)
+  
